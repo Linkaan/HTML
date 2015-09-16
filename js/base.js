1b3966482f70bb5b3dfac152ffd3ee4daa731091
Linus Styrén <linus122xbb@gmail.com>
2015-09-16 18:33:53 +0200

    Fixade bugg och lade till bättre bakgrundsbild!

--- js/base.js
+++ js/base.js
@@ -2,28 +2,32 @@ function random(min, max) {
 	return Math.floor(Math.random() * max) + min;
 }
 
-var dead = false;
+var clicked = false;
+var isMoving = false;
 
 function moveButton(button) {
 	setTimeout(function() {
-		if (dead) return;
-		button.style.top = random(0, window.innerHeight - 100) + 'px';
-		button.style.left = random(0, window.innerWidth - 100) + 'px';
+		if (clicked || isMoving) return;
+
+		isMoving = true;
+		button.style.top = random(0, window.innerHeight - 50) + 'px';
+		button.style.left = random(0, window.innerWidth - 50) + 'px';
 	
 		button.innerHTML = "😱";
 		
 		setTimeout(function() {
-			if (dead) return;
+			isMoving = false;
+			if (clicked) return;
 			button.innerHTML = "😀";
 		}, 450);
 	}, 100);
 }
 
 function buttonClick(button) {
-	dead = true;
+	clicked = true;
 	button.innerHTML = "😔";
 	setTimeout(function() {
-		dead = false;
+		clicked = false;
 		button.innerHTML = "😀";
 		moveButton(button);
 	}, 2500);
